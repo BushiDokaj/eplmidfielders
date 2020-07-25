c0f3e65fb9f18068fb01bd18455d4018e3709858
Bushi <kujtim.dokaj@mail.utoronto.ca>
2020-07-24 21:56:20 -0400

    began work on the web app

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=analysis_app.py
+FLASK_ENV=development
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import render_template, request, jsonify, make_response, redirect, url_for, session
+import pandas as pd
+
+@app.route('/', methods=["GET"])
+@app.route('/home', methods=["GET"])
+def home():
+    return render_template('base.html')
